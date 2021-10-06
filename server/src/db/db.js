cf5772b7b7f75d2b5677863af0a5873fb63e2488
Ahmad Ali <ahmadalibaloch@gmail.com>
2021-10-06 15:19:04 +0500

    fix: set the db to inmemory for easy testing refactor: add demo

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -2,13 +2,15 @@
 // DB layer
 const { pubsub, SYNC_DB } = require('../servers/pubsub');
 const { Sequelize } = require('sequelize');
-const connString = `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`;
-const sequelize = new Sequelize(connString);
+// const connString = `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`;
+const sequelize = new Sequelize("sqlite::memory:");
+
 
 
 // test connection
 (async () => {
 	try {
+		console.log('Connecting db...');
 		await sequelize.authenticate();
 		console.log('Connection has been established successfully.');
 		pubsub.publish(SYNC_DB);
